4b7b128ab50b7e3d93edd12277c40ba56c49f41d
spreered <spreered@gmail.com>
2025-02-23 23:07:55 +0800

    Transfer from Old blog

--- src/consts.ts
+++ src/consts.ts
@@ -13,25 +13,26 @@ export type Link = {
 }
 
 export const SITE: Site = {
-  TITLE: 'astro-erudite',
+  TITLE: 'SpreeLab',
   DESCRIPTION:
-    'astro-erudite is a opinionated, no-frills blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  EMAIL: 'jason@enscribe.dev',
-  NUM_POSTS_ON_HOMEPAGE: 2,
-  POSTS_PER_PAGE: 3,
-  SITEURL: 'https://astro-erudite.vercel.app',
+    'Programmable Life',
+  EMAIL: 'spreered@gmail.com',
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  POSTS_PER_PAGE: 10,
+  SITEURL: 'https://wwww.spreered.com',
 }
 
 export const NAV_LINKS: Link[] = [
   { href: '/blog', label: 'blog' },
-  { href: '/authors', label: 'authors' },
   { href: '/about', label: 'about' },
   { href: '/tags', label: 'tags' },
 ]
 
 export const SOCIAL_LINKS: Link[] = [
-  { href: 'https://github.com/jktrn', label: 'GitHub' },
-  { href: 'https://twitter.com/enscry', label: 'Twitter' },
-  { href: 'jason@enscribe.dev', label: 'Email' },
+  { href: 'https://github.com/spreered', label: 'GitHub' },
+  { href: 'https://x.com/spreered23', label: 'Twitter' },
+  { href: 'https://www.linkedin.com/in/li-hsuan-hung-998148160/', label: 'LinkedIn' },
+  { href: 'https://https://medium.com/@spreered', label: 'Medium' },
+  { href: 'mailto:spreered@gmail.com', label: 'Email' },
   { href: '/rss.xml', label: 'RSS' },
 ]
---
+++ src/content/authors/spreered.md
@@ -0,0 +1,10 @@
+---
+name: 'Spreered'
+pronouns: 'Fred Hung'
+avatar: 'https://1.gravatar.com/avatar/44943c65957446da1488f9ff7af5620b7cde9b7ab93556208d066d5efd3c8105?size=256'
+bio: '我不是半途而廢，只是走得比較慢'
+website: 'https://www.spreered.com'
+twitter: 'https://twitter.com/spreered23'
+github: 'https://github.com/spreered'
+mail: 'spreered@gmail.com'
+---
